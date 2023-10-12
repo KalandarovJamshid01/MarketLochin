7a69f5d76831e7a7ca903730baa80b60f969362c
Jamshid <kalandarovjamshid01@gmail.com>
2023-10-12 11:03:48 +0500

    okay, cors

--- middleware/app.js
+++ middleware/app.js
@@ -29,19 +29,7 @@ app.use("/download", express.static("public"));
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "http://localhost:5173",
-      "http://127.0.0.1:3000",
-      "http://127.0.0.1:3001",
-      "http://127.0.0.1:5173",
-      "https://magazines-saller.vercel.app/",
-      "https://magazines-admin.vercel.app/",
-      "https://marketlochin.uz/",
-      "https://admin.marketlochin.uz/",
-      "*",
-    ],
+    origin: "*",
     credentials: true,
   })
 );
