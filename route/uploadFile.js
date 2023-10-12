00fa29f63daf0878ccc3c6c9d6bac0f28bad686b
Jamshid <kalandarovjamshid01@gmail.com>
2023-10-12 13:06:14 +0500

    okay

--- route/uploadFile.js
+++ route/uploadFile.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const { protect } = require("../controller/verify");
 const uploadFile = require("./../controller/uploadFile");
 
 router
   .route("/")
-  .post(protect, uploadFile.upload.single("file"), uploadFile.uploadFile)
+  .post( uploadFile.upload.single("file"), uploadFile.uploadFile)
   .get();
 module.exports = router;
