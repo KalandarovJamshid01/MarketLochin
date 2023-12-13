a62a0166e267eb04670e3bfc1ae345a66cb952dc
Jamshid <kalandarovjamshid01@gmail.com>
2023-12-13 11:39:20 +0500

    okay, sms text

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,12 +1,12 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 404;
-  err.status = err.status || "fail";
-  err.message = err.message || "not found";
-  if (err.message === "invalid signature") {
-    err.message = "Your token is not valid!";
+  err.status = err.status || 'fail';
+  err.message = err.message || 'not found';
+  if (err.message === 'invalid signature') {
+    err.message = 'Your token is not valid!';
   }
-  if (process.env.NODE_ENV === "DEVELOPMENT") {
-    if (req.originalUrl.startsWith("/api/")) {
+  if (process.env.NODE_ENV === 'DEVELOPMENT') {
+    if (req.originalUrl.startsWith('/api/')) {
       res.status(err.statusCode).json({
         status: err.status,
         error: err,
@@ -14,19 +14,19 @@ module.exports = (err, req, res, next) => {
         stack: err.stack,
       });
     } else {
-      res.status(err.statusCode).render("error", {
+      res.status(err.statusCode).render('error', {
         message: err.message,
       });
     }
-  } else if (process.env.NODE_ENV === "PRODUCTION") {
-    if (req.originalUrl.startsWith("/api/")) {
+  } else if (process.env.NODE_ENV === 'PRODUCTION') {
+    if (req.originalUrl.startsWith('/api/')) {
       res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
       });
     } else {
-      res.status(err.statusCode).render("error", {
-        message: "Please try again !",
+      res.status(err.statusCode).render('error', {
+        message: 'Please try again !',
       });
     }
   }
